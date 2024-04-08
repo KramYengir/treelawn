a316b584c391e490b17247b91975578a7ac5cf82
KramYengir <kramyengir@gmail.com>
2024-04-08 15:08:02 +0100

    add basic responsive header & nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header/Header";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import "./Header.css";
+import { IoIosMenu } from "react-icons/io";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header>
+      <nav className="container">
+        <div className="logo">TreeLawn</div>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <IoIosMenu />
+        </button>
+        {isOpen && (
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Get in Touch!</a>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </header>
+  );
 };
 
 export default Header;

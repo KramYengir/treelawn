40f6be840df46c1e976e8d0c80af6c47a17cf3c1
KramYengir <kramyengir@gmail.com>
2024-04-09 17:41:49 +0100

    remove padding rule from container class

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Contact />
     </>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <section className="container contact">
+      <h2>have a question? get in touch!</h2>
+    </section>
+  );
+};
+
+export default Contact;
